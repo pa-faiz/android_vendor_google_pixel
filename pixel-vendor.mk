0ab9731ab88f98a41f73176db7814f0646dad8df
NurKeinNeid <mralexman3000@gmail.com>
2025-01-07 21:49:25 +0100

    pixel: Remove SystemUIClocks-Weather
    Change-Id: I09ab0b76ace2a4664f7533ddbb54e2911c163e4d
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- pixel-vendor.mk
+++ pixel-vendor.mk
@@ -116,5 +116,4 @@ PRODUCT_PACKAGES += \
     SystemUIClocks-Growth \
     SystemUIClocks-Inflate \
     SystemUIClocks-Metro \
-    SystemUIClocks-NumOverlap \
-    SystemUIClocks-Weather
+    SystemUIClocks-NumOverlap
